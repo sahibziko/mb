d1cd7e9acf7eb03886c723fa6eeb4ac9ff6c5772
RaviVeyi <112951109+RaviVeyi@users.noreply.github.com>
2023-07-05 12:02:32 +0400

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/fec9e97f7b3b7ea8d65ba.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/fec9e97f7b3b7ea8d65ba.jpg")
+PING_IMG = getenv("PING_IMG")
+START_IMG = getenv("START_IMG")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/teleqramchat")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Teleqrambots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5519651365").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = ""
